07e680cd0421c190690fdb8ac03d0c9a9e6e4dc1
Garry <personarpg7501@gmail.com>
2023-12-20 20:02:57 +0900

    check : fs 모듈을 이용하여 index.html 파일 생성, 이때 html의 틀을 만들고 content의 내용을 body에 넣고 랜더링

--- server.js
+++ server.js
@@ -4,3 +4,4 @@ const fs = require('fs');
 
 const content = '<h1>안녕 세계</h1>';
 
+fs.writeFileSync('index.html', `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>SSG Hello World</title></head><body>${content}</body></html>`);
